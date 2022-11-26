6df87a2f83cd5515bc78e77752dbf3861eccfb6c
Kyle Harrison <redactedprofile@gmail.com>
2022-11-26 14:41:29 -0800

    No longer need to include the controller files manually in the index.php file

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@ if($_SERVER['SCRIPT_NAME'] !== '/index.php') {
     return false;
 }
 
+// Include Composer Files
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Include System Files
 require_once __DIR__ . '/../src/bootstrap.php';
 
 $matched_route = Core\Router\Router::Get()->match(Core\Http\Request::Get()->getServer()->getString('PATH_INFO'));
@@ -14,11 +16,6 @@ if(!$matched_route) {
     echo "404 not found";
     die();
 }
-
-// @todo Can automate this with a glob to the controller directory, so the user doesn't need to sit here and do this for ever new controller authored
-require_once __DIR__ . '/../src/App/Controllers/HomeController.php';
-require_once __DIR__ . '/../src/App/Controllers/PostController.php';
-require_once __DIR__ . '/../src/App/Controllers/SecurityController.php';
 
 $controller = new $matched_route['controller']();
 $response = call_user_func_array([$controller, $matched_route['action']], $matched_route['parameters']);
